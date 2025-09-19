2868f64097ee05f46f62d5853bb9acd6be03b88d
Niharika <niharikasi725@gmailcom>
2025-09-19 13:18:06 +0530

    Print table of 9 using printf() without using loop

---
+++ src/Print_Table.java
@@ -0,0 +1,23 @@
+public class Print_Table {
+
+    public static void main(String[] args){
+
+        int a=9;
+
+        //Print table of 9 with printf() without using any loop
+
+        System.out.printf("%d X 1 = %d %n",a,a*1);
+        System.out.printf("%d X 2 = %d %n",a,a*2);
+        System.out.printf("%d X 3 = %d %n",a,a*3);
+        System.out.printf("%d X 4 = %d %n",a,a*4);
+        System.out.printf("%d X 5 = %d %n",a,a*5);
+        System.out.printf("%d X 6 = %d %n",a,a*6);
+        System.out.printf("%d X 7 = %d %n",a,a*7);
+        System.out.printf("%d X 8 = %d %n",a,a*8);
+        System.out.printf("%d X 9 = %d %n",a,a*9);
+        System.out.printf("%d X 10 = %d %n",a,a*10);
+
+    }
+
+
+}
