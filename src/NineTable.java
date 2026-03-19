2ae123e45038758a90ba875ea1bce47225145977
NiharikaSingh725 <Niharikasi725@gmail.com>
2026-03-19 19:58:27 +0530

    commit the changes

---
+++ src/NineTable.java
@@ -0,0 +1,17 @@
+public class NineTable {
+    public static void main(String[] args){
+        int a=9;
+        System.out.printf("%dx1=%d%n",a,a*1);
+        System.out.printf("%dX2=%d%n",a,a*2);
+        System.out.printf("%dX3=%d%n",a,a*3);
+        System.out.printf("%dX4=%d%n",a,a*4);
+        System.out.printf("%dX5=%d%n",a,a*5);
+        System.out.printf("%dX6=%d%n",a,a*6);
+        System.out.printf("%dX7=%d%n",a,a*7);
+        System.out.printf("%dX8=%d%n",a,a*8);
+        System.out.printf("%dX9=%d%n",a,a*9);
+        System.out.printf("%dX10=%d",a,a*10);
+
+
+    }
+}
